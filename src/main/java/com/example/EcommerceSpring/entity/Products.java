66054fc7f27a6f7d20998a2028355a343a2b9e95
NayabJalal <jmd341602@gmail.com>
2025-10-28 19:09:53 +0530

    Fixed Errors

--- src/main/java/com/example/EcommerceSpring/entity/Products.java
+++ src/main/java/com/example/EcommerceSpring/entity/Products.java
@@ -1,6 +1,6 @@
 package com.example.EcommerceSpring.entity;
 
-
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.*;
 
@@ -10,12 +10,15 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-
 public class Products extends BaseEntity {
-    //private Long id; -> it will be created am(BaseEntity).
+    @Column(nullable = false, length = 500)
     private String title;
+    @Column(nullable = false)
     private Double price;
+    @Column(columnDefinition = "TEXT")
     private String description;
+    @Column(length = 100)
     private String category;
+    @Column(length = 500)
     private String image;
 }
